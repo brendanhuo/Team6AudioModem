60b97852330c96c78dde9ebc23fa027b5669f1bc
byh22 <byh22@cam.ac.uk>
2021-05-19 21:05:15 +0100

    Add chirp to sim channel

--- Brendan/simulated_channel.py
+++ Brendan/simulated_channel.py
@@ -9,6 +9,8 @@ import soundfile as sf
 import pandas as pd
 import bitarray 
 import binascii
+import math 
+from scipy import signal
 
 N = 1024 # DFT size
 K = 512 # number of OFDM subcarriers with information
@@ -31,12 +33,35 @@ H = np.fft.fft(channelResponse, N)
 #plt.xlabel('FT index'); plt.ylabel('$|H(f)|$'); plt.grid(True); plt.xlim(0, N)
 #plt.show()
 
+###IMPORT TEXT FILE FOR TESTING
 with open("./Brendan/test_file.txt") as f:
     contents = f.read()
 
 import bitarray
 ba = bitarray.bitarray()
 ba.frombytes(contents.encode('utf-8'))
+
+ba = np.array(ba.tolist())
+
+###IMPORT CHIRP###
+def exponential_chirp(T=10.0, f1=60.0, f2=6000.0, window_strength=10.0, fs=44100):
+    """Produces chirp and returns impulse characteristics"""
+
+    t_list = np.linspace(0, T, int(round(T * fs)), False)
+    profile = []
+    r = f2/f1
+
+    # Calculate Sine Sweep time domain values
+    for t in t_list:
+        value = math.sin(2*math.pi*T*f1*((r**(t/T)-1)/(math.log(r, math.e))))*(1-math.e**(-window_strength*t))*(1-math.e**(window_strength*(t-T)))
+        profile.append(value)
+
+    # Format
+    profile = np.array(profile)
+
+    return profile
+exponentialChirp = exponential_chirp()
+#print(exponentialChirp)
 
 #QPSK
 mu = 2 # bits per symbol 
@@ -49,8 +74,6 @@ mapping_table = {
     (1,1) : -1-1j
 }
 demapping_table = {v : k for k, v in mapping_table.items()}
-
-ba = np.array(ba.tolist())
 
 #append required number of zeros to end to send a full final OFDM symbol
 ba = np.append(ba, np.zeros(len(dataCarriers)*2 - (len(ba) - len(ba)//mu//len(dataCarriers) * len(dataCarriers) * mu)))
@@ -88,6 +111,8 @@ def mapToTransmit(bits_SP):
 
 sound = mapToTransmit(bits_SP)
 #print(len(sound))
+
+#SIMULATED CHANNEL
 def channel(signal, channelResponse, SNRdb=20):
     convolved = np.convolve(signal, channelResponse)
     signal_power = np.mean(abs(convolved**2))
@@ -101,14 +126,79 @@ def channel(signal, channelResponse, SNRdb=20):
 
 OFDM_TX = sound
 OFDM_RX = channel(OFDM_TX, channelResponse)
-plt.figure(figsize=(8,2))
-plt.plot(abs(OFDM_TX), label='TX signal')
-plt.plot(abs(OFDM_RX), label='RX signal')
-plt.legend(fontsize=10)
-plt.xlabel('Time'); plt.ylabel('$|x(t)|$');
-plt.grid(True);
-plt.show()
+#plt.figure(figsize=(8,2))
+#plt.plot(abs(OFDM_TX), label='TX signal')
+#plt.plot(abs(OFDM_RX), label='RX signal')
+#plt.legend(fontsize=10)
+#plt.xlabel('Time'); plt.ylabel('$|x(t)|$');
+#plt.grid(True);
+#plt.show()
 #print(len(OFDM_RX))
+
+chirp_filtered = channel(exponentialChirp, channelResponse)
+
+#Add some zeros in front to test synchronisation
+chirp_filtered = np.append(np.zeros(10000), chirp_filtered)
+
+def chirp_estimator_test(audio, samples=30, f1=60.0, f2=6000.0, T=5, fs=44100, actualH = H):
+    x = exponential_chirp(T)
+    x_r = x[::-1]
+
+    # Apply exponential envelope and normalise
+    for i in range(len(x_r)):
+        x_r[i] = x_r[i] * math.e ** (-(i / (T * fs)) * math.log2(f2 / f1))
+
+    x_r = x_r / np.linalg.norm(x_r)
+
+    y = audio / np.linalg.norm(audio)
+    # Convolve output with x_r
+    h = signal.fftconvolve(x_r, y)
+
+    # Estimate Impulse Response start point
+    position = np.where(h == np.amax(h))[0][0]
+    
+    # Find closest 0 to estimate above and set as Impulse Response start point
+    i = 1
+    while True:
+        if 0 <= h[position - i] <= h[position - i + 1]:
+            i += 1
+        else:
+            break
+    position -= i
+    if abs(h[position]) > abs(h[position - 1]):
+        position -= 1
+    h[position] = 0
+
+    # Duplicate and truncate h
+    h_0 = h
+    h = h[position:int(position + samples)]
+
+    H2 = np.fft.fft(h, N)
+
+    plt.plot(np.linspace(0, len(h) / fs, len(h), False), h)
+    plt.title('Measured Impulse Response')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Relative Amplitude')
+    plt.grid(True)
+    plt.show()
+
+    plt.plot(np.linspace(0, len(h_0) / fs, len(h_0), False), h_0)
+    plt.title('Full Recording')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Relative Amplitude')
+    plt.grid(True)
+    plt.show()
+    
+    #plt.semilogy(np.linspace(0, fs, len(H2), False), abs(H2), label = 'measured H')
+    plt.plot(np.arange(len(H2)), abs(H2), label = 'measured H')
+    plt.plot(np.arange(N), abs(actualH), label = 'actual H')
+    plt.xlabel('FT index'); plt.ylabel('$|H(f)|$'); plt.grid(True); plt.xlim(0, N); plt.legend()
+    plt.show()
+    return h, H2, position
+
+h2, H2, position = chirp_estimator_test(chirp_filtered)
+
+print(position)
 
 def removeCP(signal, a):
     return signal[a:(a+N)]
