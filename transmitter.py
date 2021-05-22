032b1aeab0c8c305644fd4e410688aff8cd6c890
Jamesmcozens <84142175+Jamesmcozens@users.noreply.github.com>
2021-05-22 21:31:04 +0100

    Changed placement of definitions for convenience

--- transmitter.py
+++ transmitter.py
@@ -9,19 +9,25 @@ import math, cmath
 from scipy.io.wavfile import write
 from numpy.random import default_rng
 
-""" Define the data carriers and pilot tone carriers
-# N - the DFT size
-# K - number of OFDM subcarriers with information
-# P - number of pilot tones per OFDM block """
+
 def assign_data_pilot(K, P):
+    """ Define the data carriers and pilot tone carriers
+    # N - the DFT size
+    # K - number of OFDM subcarriers with information
+    # P - number of pilot tones per OFDM block """
+
     allCarriers = np.arange(K)
     pilotCarriers = allCarriers[1::K//P]
     dataCarriers = np.delete(allCarriers, pilotCarriers)
     dataCarriers = np.delete(dataCarriers, 0)
+    print(allCarriers, pilotCarriers, dataCarriers)
+
     return dataCarriers, pilotCarriers
 
-""" Maps each batch of bits to a constellation symbol"""
+
 def mapping(bits):
+    """ Maps each batch of bits to a constellation symbol"""
+
     mapping_table = {
         (0,0) : 1+1j,
         (0,1) : -1+1j,
@@ -29,27 +35,38 @@ def mapping(bits):
         (1,1) : -1-1j
     }
     demapping_table = {v : k for k, v in mapping_table.items()}
+
     return np.array([mapping_table[tuple(b)] for b in bits])
 
-"""Assigns pilot values and payload values to OFDM symbol, take reverse complex conjugate and append to end to make signal passband"""
+
 def ofdm_symbol(K, pilotValue, pilotCarriers, dataCarriers, qpskPayload):
+    """Assigns pilot values and payload values to OFDM symbol, take reverse complex conjugate and append to end to make signal passband"""
+
     symbol = np.zeros(K, dtype=complex) # the overall K subcarriers
     symbol[pilotCarriers] = pilotValue  # allocate the pilot subcarriers 
     symbol[dataCarriers] = qpskPayload  # allocate the data subcarriers
     symbol = np.append(symbol, np.append(0, np.conj(symbol)[:0:-1]))
+
     return symbol
 
-"""Take IDFT"""
+
 def idft(ofdmData):
+    """Take IDFT"""
+
     return np.fft.ifft(ofdmData)
 
-"""Adds cyclic prefix"""
+
 def add_cp(CP, ofdmTime):
+    """Adds cyclic prefix"""
+
     cp = ofdmTime[-CP:]               # take the last CP samples ...
+
     return np.hstack([cp, ofdmTime])  # ... and add them to the beginning
 
-"""Create full sequence to transmit through speakers"""
+
 def map_to_transmit(K, CP, pilotValue, pilotCarriers, dataCarriers, bitsSP):
+    """Create full sequence to transmit through speakers"""
+
     soundArray = []
     for section in bitsSP:
         qpsk = mapping(section)
@@ -57,10 +74,13 @@ def map_to_transmit(K, CP, pilotValue, pilotCarriers, dataCarriers, bitsSP):
         ofdmTime = idft(ofdmData)
         ofdmWithCP = add_cp(CP, ofdmTime)
         soundArray.append(ofdmWithCP.real)
+
     return np.array(soundArray).ravel()
 
-"""Known OFDM block generation for channel estimation"""
+
 def known_ofdm_block(blocknum, randomSeedStart, mu, K, CP, mappingTable):
+    """Known OFDM block generation for channel estimation"""
+
     knownOFDMBlock = []
     for i in range(blocknum):
         rng = default_rng(randomSeedStart + i)
@@ -75,4 +95,3 @@ def known_ofdm_block(blocknum, randomSeedStart, mu, K, CP, mappingTable):
         knownOFDMBlock.append(ofdmWithCP.real)
         
     return np.array(knownOFDMBlock).ravel()
-
