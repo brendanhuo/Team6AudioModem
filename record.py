1e1faef96169e548178dbf8b8c75f3973d759840
byh22 <byh22@cam.ac.uk>
2021-05-30 00:59:23 +0100

    Merge

--- record.py
+++ record.py
@@ -8,13 +8,13 @@ from channel import *
 # Comment out if not recording
 print('Recording')
 
-listening_time = 160
+listening_time = 150
 r = sd.rec(int(listening_time * fs), samplerate=fs, channels=1)
 sd.wait()  # Wait until recording is finished
-write('audio/image_received_autumn_5_ldpc.wav', fs, r)  # Save as WAV file
+write('audio/image_received_autumn_6_ldpc.wav', fs, r)  # Save as WAV file
 
 audio_received = []
 for i in range(len(r)):
     audio_received.append(r[i][0])
 
-np.save("audio/image_received_autumn_5_ldpc.npy", np.asarray(audio_received))
+np.save("audio/image_received_autumn_6_ldpc.npy", np.asarray(audio_received))
